e124f05c5fe5b2e92e68e44816a379ae3427755b
sayem118 <91573543+sayem118@users.noreply.github.com>
2022-01-22 01:20:57 +0000

    Delete clubs directory

--- clubs/management/commands/seed.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-
-class Command(BaseCommand):
-    """The database seeder."""
-
-    def handle(self, *args, **options):
-        print("TODO: The database seeder will be added here...")
