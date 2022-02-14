67381a6ea9a3afc48de4bf4c090aa3dd9a3045c5
Sayem Ahmed <sa05102001@gmail.com>
2022-02-14 14:19:51 +0000

    Add mapbox API

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:flutter_mapbox_navigation/library.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,19 +13,77 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
-  final searchController = TextEditingController();
-  final LatLng _center = const LatLng(51.5, -0.12);
+  String _platformVersion = 'Unknown';
+  final _origin = WayPoint(
+      name: "Way Point 1",
+      latitude: 51.52233,
+      longitude: 0.04330);
+  final _stop1 = WayPoint(
+      name: "Way Point 2",
+      latitude: 51.53769,
+      longitude: 0.07006);
+  final _stop2 = WayPoint(
+      name: "Way Point 3",
+      latitude: 51.55418,
+      longitude: 0.11457);
+  final _stop3 = WayPoint(
+      name: "Way Point 4",
+      latitude: 51.57794,
+      longitude: 0.20648);
+  final _stop4 = WayPoint(
+      name: "Way Point 5",
+      latitude: 51.61078,
+      longitude: 0.27956);
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
+  late MapBoxNavigation _directions;
+  late MapBoxOptions _options;
+
+  bool _isMultipleStop = false;
+  late MapBoxNavigationViewController _controller;
 
   @override
-  void dispose() {
-    // Clean up the controller when the widget is disposed.
-    searchController.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initialize() async {
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    _directions = MapBoxNavigation(onRouteEvent: _onEmbeddedRouteEvent);
+    _options = MapBoxOptions(
+        initialLatitude: 55.1175275,
+        initialLongitude: 0.4839524,
+        zoom: 15.0,
+        tilt: 0.0,
+        bearing: 0.0,
+        enableRefresh: false,
+        alternatives: true,
+        voiceInstructionsEnabled: true,
+        bannerInstructionsEnabled: true,
+        allowsUTurnAtWayPoints: true,
+        mode: MapBoxNavigationMode.cycling,
+        units: VoiceUnits.imperial,
+        simulateRoute: false,
+        animateBuildRoute: true,
+        longPressDestinationEnabled: true,
+        language: "en");
+
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await _directions.platformVersion;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
   }
 
   @override
@@ -31,28 +91,108 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: TextField(
-            controller: searchController,
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              labelText: 'Enter your destination',
+          title: const Text('Cycle planner'),
+        ),
+        body: Center(
+          child: Column(children: <Widget>[
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Text('Running on: $_platformVersion\n'),
+                    Container(
+                      color: Colors.grey,
+                      width: double.infinity,
+                      child: const Padding(
+                        padding: EdgeInsets.all(0),
+                        child: (Text(
+                          "Full Screen Navigation",
+                          style: TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center,
+                        )),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          child: const Text("Start Route"),
+                          onPressed: () async {
+                            _isMultipleStop = true;
+                            var wayPoints = <WayPoint>[];
+                            wayPoints.add(_origin);
+                            wayPoints.add(_stop1);
+                            wayPoints.add(_stop2);
+                            wayPoints.add(_stop3);
+                            wayPoints.add(_stop4);
+
+
+                            await _directions.startNavigation(
+                                wayPoints: wayPoints,
+                                options: _options);
+                          },
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
-          backgroundColor: Colors.blue[400],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        print(searchController.text);
-      },
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: Colors.pink,
+                child: MapBoxNavigationView(
+                    options: _options,
+                    onRouteEvent: _onEmbeddedRouteEvent,
+                    onCreated:
+                        (MapBoxNavigationViewController controller) async {
+                      _controller = controller;
+                      controller.initialize();
+                    }),
+              ),
+            )
+          ]),
         ),
       ),
     );
+  }
+
+  Future<void> _onEmbeddedRouteEvent(e) async {
+
+    switch (e.eventType) {
+      case MapBoxEvent.progress_change:
+        var progressEvent = e.data as RouteProgressEvent;
+        if (progressEvent.currentStepInstruction != null) {
+        }
+        break;
+      case MapBoxEvent.route_building:
+      case MapBoxEvent.route_built:
+        setState(() {
+        });
+        break;
+      case MapBoxEvent.route_build_failed:
+        setState(() {
+        });
+        break;
+      case MapBoxEvent.navigation_running:
+        setState(() {
+        });
+        break;
+      case MapBoxEvent.on_arrival:
+        if (!_isMultipleStop) {
+          await Future.delayed(const Duration(seconds: 3));
+          await _controller.finishNavigation();
+        } else {}
+        break;
+      case MapBoxEvent.navigation_finished:
+      case MapBoxEvent.navigation_cancelled:
+        setState(() {
+        });
+        break;
+      default:
+        break;
+    }
+    setState(() {});
   }
 }
