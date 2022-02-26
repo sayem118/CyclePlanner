9a0eb8be85797c3fa7d1362f7cd93c41c15e908f
Katinka-Singh <katinka.singh@kcl.ac.uk>
2022-02-26 12:59:40 +0000

    Added widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,25 +6,58 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_mapbox_navigation/library.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:cycle_planner/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('Widget Tests', () {
+    testWidgets(
+        'Group size page is loaded correctly', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // Verify that our counter starts at 1.
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
+      //Verify the correct texts are displayed
+      expect(find.text('Group size'), findsOneWidget);
+      expect(find.text('Start Route'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      //Verify the group size will not decrement to 0
+      await tester.tap(find.byIcon(Icons.remove));
+      await tester.pump();
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+
+      // Verify that our counter has incremented.
+      expect(find.text('1'), findsNothing);
+      expect(find.text('2'), findsOneWidget);
+
+      // Tap the '-' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.remove));
+      await tester.pump();
+
+      // Verify that our counter has decremented.
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
+    });
+
+    testWidgets(
+        'Route page is loaded correctly', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pump();
+
+      //Verify navigation is on page.
+      expect(find.byType(MapBoxNavigationView), findsOneWidget);
+
+    });
+
   });
 }
