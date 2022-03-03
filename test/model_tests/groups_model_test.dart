b27e5265883582822ab185f6f3b7e73ec8ca99f2
Katinka-Singh <katinka.singh@kcl.ac.uk>
2022-03-03 17:11:11 +0000

    Groups Model tests

---
+++ test/model_tests/groups_model_test.dart
@@ -0,0 +1,24 @@
+//import test package and groups model
+import 'package:test/test.dart';
+import 'package:cycle_planner/models/groups.dart';
+
+void main() {
+  test('Groups constructor sets attributes', () {
+    final mockGroup = Groups(groupSize: 3);
+    //ensure the number 3 is returned
+    expect(mockGroup.groupSize, 3);
+  });
+
+  test('getGroupSize() returns an integer', () {
+    final mockGroup = Groups(groupSize: 4);
+    //ensure the number 4 is returned
+    expect(mockGroup.getGroupSize(), 4);
+  });
+
+  test('setGroupSize works correctly', () {
+    final mockGroup = Groups(groupSize: 4);
+    mockGroup.setGroupSize(2);
+    //ensure the number 2 is returned
+    expect(mockGroup.getGroupSize(), 2);
+  });
+}
