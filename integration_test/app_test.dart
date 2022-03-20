6278a86f18b241ec05d472331880d9c1d191f9fc
Sayem Ahmed <sa05102001@gmail.com>
2022-03-20 12:47:37 +0000

    Add typo corrections

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:cycle_planner/main.dart/' as app;
+import 'package:cycle_planner/main.dart' as app;
 import 'package:cycle_planner/processes/application_processes.dart';
 
 
