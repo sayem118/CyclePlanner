cf86cc15c806bc8750095d347b805e6deb94fa7e
HadwaH <79116483+HadwaH@users.noreply.github.com>
2022-03-21 05:11:17 +0000

    Add database collections

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 Future<void>main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
