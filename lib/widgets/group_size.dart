fd039df4d8d9ec4fcac2c2f2336d71bcfb7f89c5
Ammar-alshareef <K19030887@kcl.ac.uk>
2022-03-21 14:55:21 +0000

    Refine group_size.dart

--- lib/widgets/group_size.dart
+++ lib/widgets/group_size.dart
@@ -9,20 +9,51 @@ class GroupSize extends StatefulWidget {
 }
 
 class _GroupSizeState extends State<GroupSize> {
-  int? _currentValue;
+  int _currentValue = 1;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        NumberPicker(
-          value: _currentValue!,
-          minValue: 1,
-          maxValue: 8,
-          onChanged: (value) => setState(() => _currentValue = value),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
         ),
-        Text('Current value: $_currentValue'),
-      ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 0.0),
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                decoration: BoxDecoration( // Comment decoration for transluscent effect
+                  border: Border.all(color: Colors.black)
+                ),
+                child: NumberPicker(
+                  value: _currentValue,
+                  minValue: 1,
+                  maxValue: 8,
+                  onChanged: (value) => setState(() => _currentValue = value),
+                  haptics: true,
+                  itemHeight: 90.0,
+                  itemWidth: 150.0,
+                  textStyle: const TextStyle(fontSize: 25.0),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                'Current value: $_currentValue',
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
