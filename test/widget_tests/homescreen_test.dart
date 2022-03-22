d3b1424fdf06c7cc6293bf0167279b99f816d1f6
Sayem Ahmed <sa05102001@gmail.com>
2022-03-22 15:23:35 +0000

    coverage update

--- test/widget_tests/homescreen_test.dart
+++ test/widget_tests/homescreen_test.dart
@@ -5,6 +5,28 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:cycle_planner/main.dart';
 
 void main() {
+  testWidgets('finds a widget using a Key', (WidgetTester tester) async {
+    // Define the test key.
+    const testKey = Key('K');
+
+    // Build a MaterialApp with the testKey.
+    await tester.pumpWidget(MaterialApp(key: testKey, home: Container()));
+
+    // Find the MaterialApp widget using the testKey.
+    expect(find.byKey(testKey), findsOneWidget);
+  });
+
+  testWidgets('finds a specific instance', (WidgetTester tester) async {
+    const childWidget = Padding(padding: EdgeInsets.zero);
+
+    // Provide the childWidget to the Container.
+    await tester.pumpWidget(Container(child: childWidget));
+
+    // Search for the childWidget in the tree and verify it exists.
+    expect(find.byWidget(childWidget), findsOneWidget);
+  });
+
+
   // testWidgets(
   //
   //     'Group size page is loaded correctly', (WidgetTester tester) async {
