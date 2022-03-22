00a809bed2dea9e9e817401e431fca0960af1db9
Katinka-Singh <katinka.singh@kcl.ac.uk>
2022-03-22 15:06:53 +0000

    Widget tests folder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mapbox_navigation/library.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:cycle_planner/main.dart';
 
 void main() {
---
+++ test/widget_tests/homescreen_test.dart
@@ -0,0 +1,42 @@
+import 'package:cycle_planner/Widgets/nav_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mapbox_navigation/library.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:cycle_planner/main.dart';
+
+void main() {
+  // testWidgets(
+  //
+  //     'Group size page is loaded correctly', (WidgetTester tester) async {
+  //   // Build our app and trigger a frame.
+  //    await tester.pumpWidget(const MyApp());
+  //
+  //   // // Verify that our counter starts at 1.
+  //   expect(find.byWidget(const NavBar()), findsOneWidget);
+  //   expect(find.text('1'), findsOneWidget);
+  //   //Verify the correct texts are displayed
+  //   expect(find.text('Start Route'), findsOneWidget);
+  //
+  //   //Verify the group size will not decrement to 0
+  //   await tester.tap(find.byIcon(Icons.remove));
+  //   await tester.pump();
+  //   expect(find.text('0'), findsNothing);
+  //   expect(find.text('1'), findsOneWidget);
+  //
+  //   // Tap the '+' icon and trigger a frame.
+  //   await tester.tap(find.byIcon(Icons.add));
+  //   await tester.pump();
+  //
+  //   // Verify that our counter has incremented.
+  //   expect(find.text('1'), findsNothing);
+  //   expect(find.text('2'), findsOneWidget);
+  //
+  //   // Tap the '-' icon and trigger a frame.
+  //   await tester.tap(find.byIcon(Icons.remove));
+  //   await tester.pump();
+  //
+  //   // Verify that our counter has decremented.
+  //   expect(find.text('0'), findsNothing);
+  //   expect(find.text('1'), findsOneWidget);
+  // });
+}
