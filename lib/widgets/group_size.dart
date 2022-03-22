ac7d4923e634f067be0983541da494918bd28f11
“Maya <maya.1.dejonge@kcl.ac.uk>
2022-03-22 19:32:36 +0000

    Add group size picker

--- lib/widgets/group_size.dart
+++ lib/widgets/group_size.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cycle_planner/processes/application_processes.dart';
 import 'package:provider/provider.dart';
+import 'package:numberpicker/numberpicker.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class GroupSize extends StatefulWidget {
   const GroupSize({Key? key}) : super(key: key);
@@ -11,6 +13,8 @@ class GroupSize extends StatefulWidget {
 
 class _GroupSizeState extends State<GroupSize> {
   late final ApplicationProcesses applicationProcesses;
+  int _currentValue = 1;
+  PanelController _pc1 = PanelController();
 
   @override
   void initState(){
@@ -24,45 +28,80 @@ class _GroupSizeState extends State<GroupSize> {
       appBar: AppBar(
         title: const Text('My Journey'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-                'My stops',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
+      body: SlidingUpPanel(
+        controller: _pc1,
+        panel: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0,0,0,20),
+                child: Text(
+                  'Group size',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              NumberPicker(
+                value: _currentValue,
+                minValue: 1,
+                maxValue: 10,
+                onChanged: (value) => setState(() => _currentValue = value),
+              ),
+            ],
+          ),
+        ),
+        minHeight: 0,
+        maxHeight: 300,
+        backdropEnabled: true,
+        backdropColor: Colors.black,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0,10,0,10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                    icon: Icon(Icons.group_add),
+                      onPressed: () {
+                        _pc1.open();
+                      },
+                    label: Text('Group size: $_currentValue'),
+                      ),
+                ],
               ),
             ),
-          ),
-          SizedBox(
-            height: 200,
-            child: ReorderableListView(
-              children: _getListItems(),
-              // The reorder function
-              onReorder: (oldIndex, newIndex) {
-                setState(() {
-                  if (newIndex > oldIndex) {
-                    newIndex = newIndex - 1;
-                  }
-                  final element = applicationProcesses.markers.removeAt(oldIndex);
+            SizedBox(
+              height: 600,
+              child: ReorderableListView(
+                  children: _getListItems(),
+                  // The reorder function
+                  onReorder: (oldIndex, newIndex) {
+                    setState(() {
+                      if (newIndex > oldIndex) {
+                        newIndex = newIndex - 1;
+                      }
+                      final element = applicationProcesses.markers.removeAt(oldIndex);
 
-                  applicationProcesses.markers.insert(newIndex, element);
-                });
-              }
+                      applicationProcesses.markers.insert(newIndex, element);
+                    });
+                  }
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
   List<Widget> _getListItems() => applicationProcesses.markers
-    .asMap()
-    .map((i, item) => MapEntry(i, _buildTenableListTile(i)))
-    .values
-    .toList();
+      .asMap()
+      .map((i, item) => MapEntry(i, _buildTenableListTile(i)))
+      .values
+      .toList();
 
   Widget _buildTenableListTile(int index) {
     return Dismissible(
