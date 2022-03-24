7c21fa9f960e58a9e4d5abab5f5b6a45c7895436
“Arham <arham.azhary@kcl.ac.uk>
2022-03-24 02:57:32 +0000

    Test run of different polyline code

--- test/api_test.dart
+++ test/api_test.dart
@@ -16,6 +16,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:flutter_polyline_points/src/constants.dart';
+import 'package:flutter_polyline_points/src/utils/polyline_result.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 // import 'package:permission_handler/permission_handler.dart';
 // import 'package:permission_handler_platform_interface/permission_handler_platform_interface.dart';
 
@@ -314,6 +319,25 @@ class MockGeolocatorPlatform extends Mock
       double endLongitude,
       ) =>
       42;
+
+  test('get list of coordinates from two geographical positions', () async {
+  final polylinePoints = PolylinePoints();
+  PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+  Constants.API_KEY, PointLatLng(6.5212402, 3.3679965),
+  PointLatLng(6.595680, 3.337030),
+  travelMode: TravelMode.driving);
+  assert(result.points.isNotEmpty == true);
+});
+
+test('get list of coordinates from an encoded String', () {
+print("Writing a test is very easy");
+final polylinePoints = PolylinePoints();
+List<PointLatLng> points =
+polylinePoints.decodePolyline("_p~iF~ps|U_ulLnnqC_mqNvxq`@");
+print("Answer ---- ");
+print(points);
+assert(points.length > 0);
+});
 }
 
 // void main() {
