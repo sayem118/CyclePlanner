eb43e1ac7eb2d430f574f80ac80559f34dbcac38
Sayem Ahmed <sa05102001@gmail.com>
2022-03-26 13:23:11 +0000

    update tests

--- test/model_tests/location_model_test.dart
+++ test/model_tests/location_model_test.dart
@@ -1,5 +1,7 @@
 //import test package and groups model
+import 'package:flutter/services.dart';
 import 'package:test/test.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:cycle_planner/models/location.dart';
 
 void main() {
@@ -9,5 +11,15 @@ void main() {
     expect(mockLocation.lat, 50.1109);
     //ensure the longitude is set
     expect(mockLocation.lng, 8.6821);
+  });
+
+  test('test 2', () {
+
+    Map file = {'lat': 50.1109, 'lng': 8.6821};
+
+    final user = Location.fromJson(file);
+
+    expect(user.lat, 50.1109);
+    expect(user.lng, 8.6821);
   });
 }
