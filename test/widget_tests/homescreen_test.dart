2124a0f2053b7b4148f61a71bb45f1617f6263d0
Ammar-alshareef <K19030887@kcl.ac.uk>
2022-03-26 06:08:35 +0000

    Update homescreen_test.dart
    - Add bottom nav bar tests in a group
    - Test HomeScreen widget instead of main

--- test/widget_tests/homescreen_test.dart
+++ test/widget_tests/homescreen_test.dart
@@ -1,64 +1,84 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:cycle_planner/main.dart';
+import 'package:cycle_planner/views/home_screen.dart';
+import 'package:cycle_planner/processes/application_processes.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  testWidgets(
+  group("Bottom nav bar", () {
+    Widget homeScreen =  ChangeNotifierProvider(
+      create: (context) => ApplicationProcesses(),
+      child: MaterialApp(
+        title: 'Cycle Planner',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomeScreen(),
+      ),
+    );
+
+    testWidgets(
       'Testing for widgets created using key', (WidgetTester tester) async {
-    // Define the test key.
-    const testKey = Key('K');
+      // Define the test key.
+      const testKey = Key('K');
 
-    // Build a MaterialApp with the testKey.
-    await tester.pumpWidget(MaterialApp(key: testKey, home: Container()));
+      // Build a MaterialApp with the testKey.
+      await tester.pumpWidget(MaterialApp(key: testKey, home: Container()));
 
-    // Find the MaterialApp widget using the testKey.
-    expect(find.byKey(testKey), findsOneWidget);
-  });
+      // Find the MaterialApp widget using the testKey.
+      expect(find.byKey(testKey), findsOneWidget);
+    });
 
-  testWidgets('Check child widgets are placed', (WidgetTester tester) async {
-    const childWidget = Padding(padding: EdgeInsets.zero);
+    testWidgets('Check child widgets are placed', (WidgetTester tester) async {
+      const childWidget = Padding(padding: EdgeInsets.zero);
 
-    // Provide the childWidget to the Container.
-    await tester.pumpWidget(Container(child: childWidget));
+      // Provide the childWidget to the Container.
+      await tester.pumpWidget(Container(child: childWidget));
 
-    // Search for the childWidget in the tree and verify it exists.
-    expect(find.byWidget(childWidget), findsOneWidget);
-  });
+      // Search for the childWidget in the tree and verify it exists.
+      expect(find.byWidget(childWidget), findsOneWidget);
+    });
 
-  testWidgets('Menu icon', (WidgetTester tester) async {
-    //   // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-    // Search for the menu icon in the tree and verify it exists.
-    await tester.tap(find.byIcon(Icons.menu));
-  });
+    testWidgets('Menu icon', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(homeScreen);
 
-  testWidgets('Directions Bike icon', (WidgetTester tester) async {
-    //   // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-    // Search for the directions bike icon in the tree and verify it exists.
-    await tester.tap(find.byIcon(Icons.directions_bike));
-  });
+      // Search for the menu icon in the tree and verify it exists.
+      await tester.tap(find.byIcon(Icons.menu));
+    });
 
-  testWidgets('Add icon', (WidgetTester tester) async {
-    //   // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-    // Search for the add icon in the tree and verify it exists.
-    await tester.tap(find.byIcon(Icons.add));
-  });
+    testWidgets('Directions Bike icon', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(homeScreen);
 
-  //Navigation Rounded Icon is manually tested
+      // Search for the directions bike icon in the tree and verify it exists.
+      await tester.tap(find.byIcon(Icons.directions_bike));
+    });
 
-  testWidgets('Directions icon', (WidgetTester tester) async {
-    //   // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-    // Search for the directions icon in the tree and verify it exists.
-    await tester.tap(find.byIcon(Icons.directions));
-  });
+    testWidgets('Add icon', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(homeScreen);
 
-  testWidgets('Group icon', (WidgetTester tester) async {
-    //   // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-    // Search for the group icon in the tree and verify it exists.
-    await tester.tap(find.byIcon(Icons.group));
+      // Search for the add icon in the tree and verify it exists.
+      await tester.tap(find.byIcon(Icons.add));
+    });
+
+    //Navigation Rounded Icon is manually tested
+
+    testWidgets('Directions icon', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(homeScreen);
+
+      // Search for the directions icon in the tree and verify it exists.
+      await tester.tap(find.byIcon(Icons.directions));
+    });
+
+    testWidgets('Group icon', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(homeScreen);
+
+      // Search for the group icon in the tree and verify it exists.
+      await tester.tap(find.byIcon(Icons.group));
+    });
   });
 }
