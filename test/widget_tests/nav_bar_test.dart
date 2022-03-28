3eb93624391918d162fd023ebf4c8f8aba5f7439
Ammar-alshareef <K19030887@kcl.ac.uk>
2022-03-28 21:15:37 +0100

    Add text tests to nav_bar_test

--- test/widget_tests/nav_bar_test.dart
+++ test/widget_tests/nav_bar_test.dart
@@ -75,5 +75,57 @@ void main() {
 
       await tester.pumpAndSettle();
     });
+
+    testWidgets('Iconic Places text', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(navbar);
+
+      // Create the finder
+      final iconicPlacesText = find.text('Iconic places');
+
+      // Search for the Iconic places text in the tree and verify it exists.
+      expect(iconicPlacesText, findsOneWidget);
+
+      await tester.pumpAndSettle();
+    });
+
+    testWidgets('Saved places text', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(navbar);
+
+      // Create the finder
+      final savedPlacesText = find.text('Saved places');
+
+      // Search for the Saved places text in the tree and verify it exists.
+      expect(savedPlacesText, findsOneWidget);
+
+      await tester.pumpAndSettle();
+    });
+
+    testWidgets('Info text', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(navbar);
+
+      // Create the finder
+      final infoText = find.text('Info');
+
+      // Search for the Info text in the tree and verify it exists.
+      expect(infoText, findsOneWidget);
+
+      await tester.pumpAndSettle();
+    });
+
+    testWidgets('Exit text', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(navbar);
+
+      // Create the finder
+      final exitText = find.text('Exit');
+
+      // Search for the exit text in the tree and verify it exists.
+      expect(exitText, findsOneWidget);
+
+      await tester.pumpAndSettle();
+    });
   });
 }
