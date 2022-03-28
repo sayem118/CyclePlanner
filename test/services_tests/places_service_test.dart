b528ebf4a54929d4440e730fa0db21db66a376d3
Sayem Ahmed <sa05102001@gmail.com>
2022-03-28 01:20:35 +0100

    fix test

--- test/services_tests/places_service_test.dart
+++ test/services_tests/places_service_test.dart
@@ -1,35 +1,31 @@
-import 'dart:convert';
 import 'package:cycle_planner/models/place.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cycle_planner/models/place_search.dart';
 import 'package:cycle_planner/services/places_service.dart';
-import 'package:http/http.dart';
-import 'package:http/testing.dart';
-import 'package:mockito/mockito.dart';
-
 
 void main() {
-  PlacesService place = PlacesService();
+  final PlacesService place = PlacesService();
   group('PlacesService', () {
 
     test('returns if getPlace search works',
             () async {
 
 // Mock the API call to return a json response with http status 200 Ok //
+          Place search = await place.getPlace("ChIJc2nSALkEdkgRkuoJJBfzkUI");
+          //const String key = 'AIzaSyDHP-Fy593557yNJxow0ZbuyTDd2kJhyCY';
+          //const String placeId = 'Riverside Building, County Hall, London SE1 7PB, UK';
+          //const double lat = 51.5033;
+          //const double lng = 0.1196;
+          //final response = await place.getResponse('ChIJc2nSALkEdkgRkuoJJBfzkUI', 'result');
+          //var search = await place.getPlace("ChIJc2nSALkEdkgRkuoJJBfzkUI");
+          //String url = 'https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJc2nSALkEdkgRkuoJJBfzkUI&location=51.5033,0.1196&key=AIzaSyDHP-Fy593557yNJxow0ZbuyTDd2kJhyCY';
+          //https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJc2nSALkEdkgRkuoJJBfzkUI&location=51.5033,0.1196&key=AIzaSyDHP-Fy593557yNJxow0ZbuyTDd2kJhyCY
+          //https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyDHP-Fy593557yNJxow0ZbuyTDd2kJhyCY&place_id=ChIJc2nSALkEdkgRkuoJJBfzkUI
 
-          final test =((await place.getPlace("temple")));
-          const String key = 'AIzaSyDHP-Fy593557yNJxow0ZbuyTDd2kJhyCY';
-          const String placeId = 'westminster';
-          const double lat = 51.4974948;
-          const double lng = -0.1356583;
-          final response = await place.getResponse('https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&location=$lat,$lng&key=$key', '1');
-          String url = 'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&location=$lat,$lng&key=$key';
-//https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJc2nSALkEdkgRkuoJJBfzkUI&location=51.5033,0.1196&key=AIzaSyDHP-Fy593557yNJxow0ZbuyTDd2kJhyCY
-          await untilCalled(place.getPlace("test") as Map<String, dynamic>);
 
-// Check whether getAutocomplete function returns
-// a list of PlaceSearch
-          expect(place.getPlace("Westminster"), response);
+          // Check whether getAutocomplete function returns
+          // a list of PlaceSearch
+          expect(search, isA<Place>());
         });
 
 });
@@ -38,15 +34,6 @@ void main() {
           () async {
 
         // Mock the API call to return a json response with http status 200 Ok //
-        final mockHTTPClient = MockClient((request) async {
-
-          // Create sample response of the HTTP call //
-          final response = {
-            'text':
-            "22834 is the feet above sea level of the highest mountain"
-          };
-          return Response(jsonEncode(response), 200);
-        });
         // Check whether getAutocomplete function returns
         // a list of PlaceSearch
         expect(await place.getAutocomplete("poplar"), isA<List<PlaceSearch>>());
@@ -56,10 +43,6 @@ void main() {
 
     // Mock the API call to return an
     // empty json response with http status 404
-    final mockHTTPClient = MockClient((request) async {
-      final response = {};
-      return Response(jsonEncode(response), 404);
-    });
     expect(await place.getAutocomplete(""),
         []);
   });
