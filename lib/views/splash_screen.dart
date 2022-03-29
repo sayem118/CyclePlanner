fe1ac8c12b209fc76f2a12aac3babd1aeae57725
HadwaH <79116483+HadwaH@users.noreply.github.com>
2022-03-29 19:38:09 +0100

    Fix splash screen logo

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -4,78 +4,65 @@ import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
 
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  State<StatefulWidget> createState() => StartState();
 }
 
-
-class _SplashScreenState extends State <SplashScreen> with TickerProviderStateMixin {
-
-  late final AnimationController _controller = AnimationController(
-    duration: const Duration(seconds: 3),
-      vsync: this)..repeat();
-
-
+class StartState extends State<SplashScreen> {
   @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    _controller.dispose();
+  Widget build(BuildContext context) {
+    return initScreen(context);
   }
-  @override
-  void initState(){
-    super.initState();
 
-    Timer(const Duration(seconds: 4),
-        () => Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()))
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  startTimer() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) => HomeScreen()
+    )
     );
   }
 
+  initScreen(BuildContext context) {
 
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0x072d5e3e),
-      body: SafeArea(
+      backgroundColor: Colors.blueGrey,
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            AnimatedBuilder(
-                animation: _controller,
-                child: Container(
-                  width: 200.0,
-                  height: 200.0,
+          children: <Widget>[
+            Container(
 
-                  child: const Center(
-                    child: Image(
-                      fit: BoxFit.cover,
-                      image: AssetImage('assets/cyclebaylogoo.png'),
-                    ),
-                  ),
-                ),
-                builder: (BuildContext context, Widget? child) {
-                  return Transform.rotate(
-                    angle: _controller.value * 2.0 * math.pi,
-                        child: child,
-                  );
-                },
-            ),
-
-            SizedBox(height: MediaQuery.of(context).size.height * .08,),
-            const Align(
-              alignment: Alignment.center,
-              child: Text('Take a ride around London!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25
-                ),
+              child: Image.asset("assets/cyclebaylogoo.png",
+                width: 300,
+                height: 300,
+               fit: BoxFit.cover,
               ),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 20.0)),
+            const Text(
+              "Take a ride around London!",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 20.0)),
+            const CircularProgressIndicator(
+              backgroundColor: Colors.white,
+              strokeWidth: 1,
             )
           ],
         ),
@@ -83,3 +70,81 @@ class _SplashScreenState extends State <SplashScreen> with TickerProviderStateMi
     );
   }
 }
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
+//
+//
+// class _SplashScreenState extends State <SplashScreen> with TickerProviderStateMixin {
+//
+//   late final AnimationController _controller = AnimationController(
+//     duration: const Duration(seconds: 3),
+//       vsync: this)..repeat();
+//
+//
+//   @override
+//   void dispose() {
+//     // TODO: implement dispose
+//     super.dispose();
+//     _controller.dispose();
+//   }
+//   @override
+//   void initState(){
+//     super.initState();
+//
+//     Timer(const Duration(seconds: 4),
+//         () => Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()))
+//     );
+//   }
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: const Color(0x072d5e3e),
+//       body: SafeArea(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             AnimatedBuilder(
+//                 animation: _controller,
+//                 child: Container(
+//                   width: 200.0,
+//                   height: 200.0,
+//
+//                   child: const Center(
+//                     child: Image(
+//                       fit: BoxFit.cover,
+//                       image: AssetImage('assets/cyclebaylogoo.png'),
+//                     ),
+//                   ),
+//                 ),
+//                 builder: (BuildContext context, Widget? child) {
+//                   return Transform.rotate(
+//                     angle: _controller.value * 2.0 * math.pi,
+//                         child: child,
+//                   );
+//                 },
+//             ),
+//
+//             SizedBox(height: MediaQuery.of(context).size.height * .08,),
+//             const Align(
+//               alignment: Alignment.center,
+//               child: Text('Take a ride around London!',
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                 fontWeight: FontWeight.bold,
+//                 fontSize: 25
+//                 ),
+//               ),
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
