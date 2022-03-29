b9febfda783a629551fd41db13913d4f2186f04c
“Arham <arham.azhary@kcl.ac.uk>
2022-03-29 18:48:39 +0100

    RemoveMarkers test added

--- test/application_processes_tests/processes_test.dart
+++ test/application_processes_tests/processes_test.dart
@@ -3,6 +3,10 @@ import 'package:cycle_planner/processes/application_processes.dart';
 import 'package:cycle_planner/services/geolocator_service.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:cycle_planner/models/location.dart';
+import 'package:cycle_planner/models/geometry.dart';
+import 'package:cycle_planner/models/place.dart';
 
 import 'package:mockito/mockito.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
@@ -44,6 +48,45 @@ void main() {
       expect(appProcesses.polylines, []);
       expect(appProcesses.polyCoords, []);
     });
+
+    test('Remove a marker', () async {
+
+      final mockLocation = Location(lat: 50.1109, lng: 8.6821);
+      final mockGeometry = Geometry(location: mockLocation);
+      final mockPlace =
+      Place(geometry: mockGeometry, name: "Test", vicinity: "Test");
+      final markerID = mockPlace.name;
+      final marker1 = Marker(
+          markerId: MarkerId(markerID),
+          draggable: false,
+          visible: true,
+          infoWindow:
+          InfoWindow(title: mockPlace.name, snippet: mockPlace.vicinity),
+          position: LatLng(mockPlace.geometry.location.lat,
+              mockPlace.geometry.location.lng));
+
+      final mockLocation2 = Location(lat: 51.494720, lng: -0.135278);
+      final mockGeometry2 = Geometry(location: mockLocation2);
+      final mockPlace2 =
+      Place(geometry: mockGeometry2, name: "Test2", vicinity: "Test2");
+      final markerID2 = mockPlace2.name;
+      final marker2 = Marker(
+          markerId: MarkerId(markerID2),
+          draggable: false,
+          visible: true,
+          infoWindow:
+          InfoWindow(title: mockPlace2.name, snippet: mockPlace2.vicinity),
+          position: LatLng(mockPlace2.geometry.location.lat,
+              mockPlace2.geometry.location.lng));
+      final addedMarker = appProcesses.markers;
+
+      addedMarker.add(marker1);
+      appProcesses.removeMarker(0);
+
+      expect(addedMarker, []);
+
+    });
+
   });
 }
 
