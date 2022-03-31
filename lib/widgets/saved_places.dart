220127adf7813b19bc95f9d8d7dfb206e3063cb7
b <b.g.almakky@gmail.com>
2022-03-31 23:01:08 +0100

    added saved places screen

--- lib/widgets/saved_places.dart
+++ lib/widgets/saved_places.dart
@@ -1,24 +1,24 @@
-//
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-//
-// import 'fetchData.dart';
-//
-// class SavedPlaces extends StatefulWidget {
-//   @override
-//   _SavedPlacesState createState() => _SavedPlacesState();
-//   }
-//
-//   class _SavedPlacesState extends State<SavedPlaces> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//   body: SafeArea(
-//   child: fetchData("users-favourite-places"),
-//   ),
-//   );
-//   }
-//   }
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'fetchData.dart';
+
+class SavedPlaces extends StatefulWidget {
+  @override
+  _SavedPlacesState createState() => _SavedPlacesState();
+  }
+
+  class _SavedPlacesState extends State<SavedPlaces> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+  body: SafeArea(
+  child: fetchData("users-favourite-places"),
+  ),
+  );
+  }
+  }
 //
 //
 //
