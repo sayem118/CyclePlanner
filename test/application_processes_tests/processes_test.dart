4321fd43bb5c785ab1c065ba570f3568c0d31cd4
“Arham <arham.azhary@kcl.ac.uk>
2022-03-31 19:12:42 +0100

    Deleted API test file

--- test/application_processes_tests/processes_test.dart
+++ test/application_processes_tests/processes_test.dart
@@ -19,42 +19,42 @@ final appProcesses = ApplicationProcesses();
 void main() {
 
   group('MarkerService', () {
-    // test('setGroupSize', () async {
-    //
-    //   appProcesses.setGroupSize(2);
-    //   //ensure the number 2 is set
-    //   expect(appProcesses.groupSize, 2);
-    // });
-    //
-    // test('Remove Polylines', () async {
-    //   appProcesses.removePolyline();
-    //
-    //   expect(appProcesses.polylines, []);
-    //   expect(appProcesses.polyCoords, []);
-    // });
+    test('setGroupSize', () async {
 
-    // test('Remove a marker', () async {
-    //   final mockLocation = Location(lat: 50.1109, lng: 8.6821);
-    //   final mockGeometry = Geometry(location: mockLocation);
-    //   final mockPlace =
-    //   Place(geometry: mockGeometry, name: "Test", vicinity: "Test");
-    //   final markerID = mockPlace.name;
-    //   final marker1 = Marker(
-    //       markerId: MarkerId(markerID),
-    //       draggable: false,
-    //       visible: true,
-    //       infoWindow:
-    //       InfoWindow(title: mockPlace.name, snippet: mockPlace.vicinity),
-    //       position: LatLng(mockPlace.geometry.location.lat,
-    //           mockPlace.geometry.location.lng));
-    //
-    //   final addedMarker = appProcesses.markers;
-    //
-    //   addedMarker.add(marker1);
-    //   appProcesses.removeMarker(0);
-    //
-    //   expect(addedMarker, []);
-    // });
+      appProcesses.setGroupSize(2);
+      //ensure the number 2 is set
+      expect(appProcesses.groupSize, 2);
+    });
+
+    test('Remove Polylines', () async {
+      appProcesses.removePolyline();
+
+      expect(appProcesses.polylines, []);
+      expect(appProcesses.polyCoords, []);
+    });
+
+    test('Remove a marker', () async {
+      final mockLocation = Location(lat: 50.1109, lng: 8.6821);
+      final mockGeometry = Geometry(location: mockLocation);
+      final mockPlace =
+      Place(geometry: mockGeometry, name: "Test", vicinity: "Test");
+      final markerID = mockPlace.name;
+      final marker1 = Marker(
+          markerId: MarkerId(markerID),
+          draggable: false,
+          visible: true,
+          infoWindow:
+          InfoWindow(title: mockPlace.name, snippet: mockPlace.vicinity),
+          position: LatLng(mockPlace.geometry.location.lat,
+              mockPlace.geometry.location.lng));
+
+      final addedMarker = appProcesses.markers;
+
+      addedMarker.add(marker1);
+      appProcesses.removeMarker(0);
+
+      expect(addedMarker, []);
+    });
 
     test('testing the Search results', () async {
       await appProcesses.searchPlaces("Westminster");
