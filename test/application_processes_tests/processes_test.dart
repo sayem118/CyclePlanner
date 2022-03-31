d90600b97009f620a6ae156120de33c346486c01
“Arham <arham.azhary@kcl.ac.uk>
2022-03-31 14:32:23 +0100

    Toggle marker test added

--- test/application_processes_tests/processes_test.dart
+++ test/application_processes_tests/processes_test.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
 import 'package:cycle_planner/processes/application_processes.dart';
+import 'package:cycle_planner/services/marker_service.dart';
+import 'package:cycle_planner/services/places_service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -12,18 +15,19 @@ import 'package:cycle_planner/models/place_search.dart';
 import 'package:mockito/mockito.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-Position get mockPosition => Position(
-    latitude: 55.561270,
-    longitude: 0.17639382,
-    timestamp: DateTime.fromMillisecondsSinceEpoch(
-      500,
-      isUtc: true,
-    ),
-    altitude: 3000.0,
-    accuracy: 0.0,
-    heading: 0.0,
-    speed: 0.0,
-    speedAccuracy: 0.0);
+Position get mockPosition =>
+    Position(
+        latitude: 55.561270,
+        longitude: 0.17639382,
+        timestamp: DateTime.fromMillisecondsSinceEpoch(
+          500,
+          isUtc: true,
+        ),
+        altitude: 3000.0,
+        accuracy: 0.0,
+        heading: 0.0,
+        speed: 0.0,
+        speedAccuracy: 0.0);
 final appProcesses = ApplicationProcesses();
 
 void main() {
@@ -71,19 +75,86 @@ void main() {
 
       expect(appProcesses.searchResults, isA<List<PlaceSearch>>());
     });
+
+    test('setLocationSelected', () async {
+      await appProcesses.setSelectedLocation("ChIJc2nSALkEdkgRkuoJJBfzkUI");
+
+      expect(appProcesses.searchResults, []);
+      expect(appProcesses.selectedLocation, isA<StreamController<Place>>());
+    });
+
+    test('toggleMarker', () async {
+      appProcesses.toggleMarker("westminster");
+
+      expect(appProcesses.placeName, "westminster");
+    });
+
+    test('', () async {
+      PlacesService place = PlacesService();
+      final markerService = MarkerService();
+
+      appProcesses.toggleMarker("westminster");
+
+
+      expect(appProcesses.placeName, "westminster");
+
+      final mockLocation = Location(lat: 50.1109, lng: 8.6821);
+      final mockGeometry = Geometry(location: mockLocation);
+      appProcesses.selectedLocationStatic =
+          Place(geometry: mockGeometry, name: "Test", vicinity: "Test");
+      Place place1 = await place.getPlaceMarkers(
+          appProcesses.selectedLocationStatic!.geometry.location.lat,
+          appProcesses.selectedLocationStatic!.geometry.location.lng,
+          appProcesses.placeName!);
+
+      var newMarker = markerService.createMarkerFromPlace(place1);
+      appProcesses.markers.add(newMarker);
+
+      expect(appProcesses.markers, isA<List<Marker>>);
+    });
+
+    test('setLocationSelected', () async {
+      await appProcesses.setSelectedLocation("ChIJc2nSALkEdkgRkuoJJBfzkUI");
+
+      expect(appProcesses.searchResults, []);
+      expect(appProcesses.selectedLocation, isA<StreamController<Place>>());
+    });
+
+    test('toggleMarker', ()  {
+      appProcesses.toggleMarker("westminster");
+
+      expect(appProcesses.placeName, "westminster");
+    });
+
+    // test('', () async {
+    //   PlacesService place = PlacesService();
+    //   final markerService = MarkerService();
+    //
+    //   appProcesses.toggleMarker("westminster");
+    //
+    //
+    //   expect(appProcesses.placeName, "westminster");
+    //
+    //   final mockLocation = Location(lat: 50.1109, lng: 8.6821);
+    //   final mockGeometry = Geometry(location: mockLocation);
+    //   appProcesses.selectedLocationStatic =
+    //       Place(geometry: mockGeometry, name: "Test", vicinity: "Test");
+    //   Place place1 = await place.getPlaceMarkers(
+    //       appProcesses.selectedLocationStatic!.geometry.location.lat,
+    //       appProcesses.selectedLocationStatic!.geometry.location.lng,
+    //       appProcesses.placeName!);
+    //
+    //   var newMarker = markerService.createMarkerFromPlace(place1);
+    //   appProcesses.markers.add(newMarker);
+    //
+    //   expect(appProcesses.markers, isA<List<Marker>>);
+
+
   });
-
-  test('setLocationSelected', () async {
-
-    await appProcesses.setSelectedLocation("ChIJc2nSALkEdkgRkuoJJBfzkUI");
-
-    expect(appProcesses.searchResults, []);
-    expect(appProcesses.selectedLocation, isA<StreamController<Place>>());
-
-  });
-
-
 }
+
+
+
 
 
 class MockGeolocatorPlatform extends Mock
@@ -161,20 +232,16 @@ class MockGeolocatorPlatform extends Mock
   Future<bool> openLocationSettings() => Future.value(true);
 
   @override
-  double distanceBetween(
-      double startLatitude,
+  double distanceBetween(double startLatitude,
       double startLongitude,
       double endLatitude,
-      double endLongitude,
-      ) =>
+      double endLongitude,) =>
       42;
 
   @override
-  double bearingBetween(
-      double startLatitude,
+  double bearingBetween(double startLatitude,
       double startLongitude,
       double endLatitude,
-      double endLongitude,
-      ) =>
+      double endLongitude,) =>
       42;
 }
