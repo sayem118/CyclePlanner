0583f7d5b71393965dd8dd60971b8a77d0ac98e1
Sayem Ahmed <sa05102001@gmail.com>
2022-03-31 16:18:52 +0100

    Update processes_test.dart

--- test/application_processes_tests/processes_test.dart
+++ test/application_processes_tests/processes_test.dart
@@ -4,7 +4,6 @@ import 'package:cycle_planner/services/marker_service.dart';
 import 'package:cycle_planner/services/places_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:cycle_planner/models/location.dart';
 import 'package:cycle_planner/models/geometry.dart';
@@ -15,26 +14,13 @@ import 'package:cycle_planner/models/place_search.dart';
 import 'package:mockito/mockito.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
-Position get mockPosition =>
-    Position(
-        latitude: 55.561270,
-        longitude: 0.17639382,
-        timestamp: DateTime.fromMillisecondsSinceEpoch(
-          500,
-          isUtc: true,
-        ),
-        altitude: 3000.0,
-        accuracy: 0.0,
-        heading: 0.0,
-        speed: 0.0,
-        speedAccuracy: 0.0);
 final appProcesses = ApplicationProcesses();
 
 void main() {
-  GeolocatorPlatform.instance = MockGeolocatorPlatform();
 
   group('MarkerService', () {
     test('setGroupSize', () async {
+
       appProcesses.setGroupSize(2);
       //ensure the number 2 is set
       expect(appProcesses.groupSize, 2);
@@ -154,95 +140,3 @@ void main() {
 
 }
 
-
-
-
-
-class MockGeolocatorPlatform extends Mock
-    with
-    // ignore: prefer_mixin
-        MockPlatformInterfaceMixin
-    implements
-        GeolocatorPlatform {
-  @override
-  Future<LocationPermission> checkPermission() =>
-      Future.value(LocationPermission.whileInUse);
-
-  @override
-  Future<LocationPermission> requestPermission() =>
-      Future.value(LocationPermission.whileInUse);
-
-  @override
-  Future<bool> isLocationServiceEnabled() => Future.value(true);
-
-  @override
-  Future<Position> getLastKnownPosition({
-    bool forceLocationManager = false,
-  }) =>
-      Future.value(mockPosition);
-
-  @override
-  Future<Position> getCurrentPosition({
-    LocationSettings? locationSettings,
-  }) =>
-      Future.value(mockPosition);
-
-  @override
-  Stream<ServiceStatus> getServiceStatusStream() {
-    return super.noSuchMethod(
-      Invocation.method(
-        #getServiceStatusStream,
-        null,
-      ),
-      returnValue: Stream.value(ServiceStatus.enabled),
-    );
-  }
-
-  @override
-  Stream<Position> getPositionStream({
-    LocationSettings? locationSettings,
-  }) {
-    return super.noSuchMethod(
-      Invocation.method(
-        #getPositionStream,
-        null,
-        <Symbol, Object?>{
-          #desiredAccuracy: locationSettings?.accuracy ?? LocationAccuracy.best,
-          #distanceFilter: locationSettings?.distanceFilter ?? 0,
-          #timeLimit: locationSettings?.timeLimit ?? 0,
-        },
-      ),
-      returnValue: Stream.value(mockPosition),
-    );
-  }
-
-  @override
-  Future<bool> openAppSettings() => Future.value(true);
-
-  @override
-  Future<LocationAccuracyStatus> getLocationAccuracy() =>
-      Future.value(LocationAccuracyStatus.reduced);
-
-  @override
-  Future<LocationAccuracyStatus> requestTemporaryFullAccuracy({
-    required String purposeKey,
-  }) =>
-      Future.value(LocationAccuracyStatus.reduced);
-
-  @override
-  Future<bool> openLocationSettings() => Future.value(true);
-
-  @override
-  double distanceBetween(double startLatitude,
-      double startLongitude,
-      double endLatitude,
-      double endLongitude,) =>
-      42;
-
-  @override
-  double bearingBetween(double startLatitude,
-      double startLongitude,
-      double endLatitude,
-      double endLongitude,) =>
-      42;
-}
