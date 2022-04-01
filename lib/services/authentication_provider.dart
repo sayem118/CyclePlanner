38c85bae3a560c2c7a44e0f776db6b7dde6c67dd
b <b.g.almakky@gmail.com>
2022-04-01 01:55:34 +0100

    Update authentication_provider.dart

--- lib/services/authentication_provider.dart
+++ lib/services/authentication_provider.dart
@@ -7,10 +7,10 @@ class AuthenticationProvider{
 // FirebaseAuth instance
   AuthenticationProvider(this.firebaseAuth);
 //Constructor to initialize the Firebase Auth instance.
-  Stream<User> get authState => firebaseAuth.idTokenChanges();
+  Stream<User?> get authState => firebaseAuth.idTokenChanges();
 
   //SIGN UP METHOD
-  Future<String> signUp({String email, String password}) async {
+  Future<String?> signUp({required String email, required String password}) async {
     try {
       await firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
@@ -21,7 +21,7 @@ class AuthenticationProvider{
   }
 
   //SIGN IN METHOD
-  Future<String> signIn({String email, String password}) async {
+  Future<String?> signIn({required String email, required String password}) async {
     try {
       await firebaseAuth.signInWithEmailAndPassword(
           email: email, password: password);
