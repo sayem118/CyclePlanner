8f6abea807b64ce08a9de3651392730488aa24f3
Ammar-alshareef <K19030887@kcl.ac.uk>
2022-04-01 19:41:58 +0100

    Add setup configuration for journey_planner_test

--- test/widget_tests/journey_planner_test.dart
+++ test/widget_tests/journey_planner_test.dart
@@ -1,12 +1,24 @@
+import 'package:cycle_planner/processes/application_processes.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cycle_planner/widgets/journey_planner.dart';
+import 'package:provider/provider.dart';
 
 void main() {
+  group('Journey Planner -', () {
+    late Widget journeyPlanner;
 
-  testWidgets('Joruney Planner created successfully', (WidgetTester tester) async {
-    JourneyPlanner testNavBar = const JourneyPlanner(
-    );
+    setUp(() {
+      journeyPlanner =  ChangeNotifierProvider(
+        create: (context) => ApplicationProcesses(),
+        child: MaterialApp(
+          title: 'Cycle Planner',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const JourneyPlanner(),
+        ),
+      );
+    });
   });
-
-
 }
