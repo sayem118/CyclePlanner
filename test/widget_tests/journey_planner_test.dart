b63bfd0c0f3653a65ee179e36bfdd22b5f2c053e
Ammar-alshareef <K19030887@kcl.ac.uk>
2022-04-02 02:59:43 +0100

    Update journey_planner_test.dart
    - Add fake marker list setup

--- test/widget_tests/journey_planner_test.dart
+++ test/widget_tests/journey_planner_test.dart
@@ -2,13 +2,29 @@ import 'package:cycle_planner/processes/application_processes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cycle_planner/widgets/journey_planner.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   group('Journey Planner -', () {
     late Widget journeyPlanner;
+    List<Marker> markerList = [];
 
     setUp(() {
+      markerList.add(
+        const Marker(
+          markerId: MarkerId("Marker 1"),
+          position: LatLng(51.508076, -0.09719399999999997)
+        )
+      );
+
+      markerList.add(
+        const Marker(
+          markerId: MarkerId("Marker 2"),
+          position: LatLng(51.5231577, -0.156863)
+        )
+      );
+
       journeyPlanner =  ChangeNotifierProvider(
         create: (context) => ApplicationProcesses(),
         child: MaterialApp(
@@ -42,5 +58,16 @@ void main() {
 
       expect(appBarText, findsOneWidget);
     });
+
+    testWidgets('Contains group size button', (WidgetTester tester) async {
+      await tester.pumpWidget(journeyPlanner);
+
+      Finder appBarText = find.text('My Journey');
+
+      expect(appBarText, findsOneWidget);
+    });
+
+
+
   });
 }
