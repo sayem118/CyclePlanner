06fe6429c7c18c667cc25291a7e68e86565efcc3
Katinka-Singh <katinka.singh@kcl.ac.uk>
2022-04-03 22:03:48 +0100

    Iconic Card Item tests - without Firebase

--- test/widget_tests/iconic_card_item_test.dart
+++ test/widget_tests/iconic_card_item_test.dart
@@ -1,26 +1,36 @@
+import 'package:cycle_planner/processes/application_processes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cycle_planner/widgets/iconic_card_item.dart';
-import 'dart:io';
+import 'package:provider/provider.dart';
 
 void main() {
+  late Widget mockCardItem;
 
-  Widget mockCardItem;
-
-  setUpAll(() {
-    HttpOverrides.global = null;
-    mockCardItem = const MaterialApp(
-        home: CardItem(
-          itemTitle: 'Test',
-          itemInfo: 'Test',
-          imageInfo: 'Test',
-          placeId: 'Test',
-          placeInfo: 'Test'
-        )
+  setUp(() async {
+    //WidgetsFlutterBinding.ensureInitialized();
+    //await Firebase.initializeApp(name: "Cycle Planner");
+    //Firebase.initializeApp(name: 'Cycle Planner', options: )
+    
+    mockCardItem =  ChangeNotifierProvider<ApplicationProcesses>(
+      create: (context) => ApplicationProcesses(),
+      child: const MaterialApp(
+          home: Scaffold (
+              body: CardItem(
+                  itemTitle: 'Test',
+                  itemInfo: 'Test',
+                  imageInfo: 'Test',
+                  placeId: 'Test',
+                  placeInfo: 'Test'
+              )
+          )
+      )
     );
   });
 
   testWidgets('CardItem created successfully', (WidgetTester tester) async {
+    //Test Card Item object
     CardItem testCardItem = const CardItem(
         itemTitle: 'Test',
         itemInfo: 'Test',
@@ -28,6 +38,28 @@ void main() {
         placeId: 'Test',
         placeInfo: 'Test'
     );
+  });
+
+  testWidgets('contains child Card', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(mockCardItem);
+
+    final cardItem = find.byType(Card);
+
+    // Search for the Card in the tree and verify it exists.
+    expect(cardItem, findsOneWidget);
+  });
+
+  //testWidgets('Testing the Future addToFavourite()', (WidgetTester tester) async {});
+
+  testWidgets('Tests for padding within Container', (WidgetTester tester) async {
+    const childWidget = Padding(padding: EdgeInsets.all(8));
+
+    // Provide the padding to the Container.
+    await tester.pumpWidget(Container(child: childWidget));
+
+    // Search for the padding in the tree and verify it exists.
+    expect(find.byWidget(childWidget), findsOneWidget);
   });
 
 }
