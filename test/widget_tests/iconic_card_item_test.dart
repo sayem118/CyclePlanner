5f9aff2fada808eaa694c60ace219477220570de
HadwaH <79116483+HadwaH@users.noreply.github.com>
2022-04-03 07:00:42 +0400

    Add iconic card group tests

--- test/widget_tests/iconic_card_item_test.dart
+++ test/widget_tests/iconic_card_item_test.dart
@@ -18,77 +18,78 @@ import 'package:provider/provider.dart';
 
     Widget mockCardItem;
 
-
-    setUp(() {
-      List<dynamic> cardItemsList = [
+    group('Iconic card -', (){
+      setUp(() {
+        List<dynamic> cardItemsList = [
           [
-             itemTitle = 'Test1',
-             itemInfo =  'Test1',
-             imageInfo = 'Test1',
-             placeId =   'Test1',
-             placeInfo = 'Test1',
+            itemTitle = 'Test1',
+            itemInfo =  'Test1',
+            imageInfo = 'Test1',
+            placeId =   'Test1',
+            placeInfo = 'Test1',
           ],
 
-        [
-          itemTitle = 'Test2',
-          itemInfo=  'Test2',
-          imageInfo='Test2',
-          placeId=  'Test2',
-          placeInfo='Test2',
-       ]
+          [
+            itemTitle = 'Test2',
+            itemInfo=  'Test2',
+            imageInfo='Test2',
+            placeId=  'Test2',
+            placeInfo='Test2',
+          ]
 
-      ];
-      cardItem =  ChangeNotifierProvider<ApplicationProcesses>(
-        create: (context) => ApplicationProcesses(),
-        child: MaterialApp(
-            title: 'Cycle Planner',
-            theme: ThemeData(
-              primarySwatch: Colors.blue,
-            ),
-            home:  Scaffold(
-              body: Column(
-                children: const [
-                  Text('testing'),
-                  CardItem(
-                    itemTitle: 'Test',
-                    itemInfo: 'Test',
-                    imageInfo: 'Test',
-                    placeId: 'Test',
-                    placeInfo: 'Test',
-                  ),
-                ],
+        ];
+        cardItem =  ChangeNotifierProvider<ApplicationProcesses>(
+          create: (context) => ApplicationProcesses(),
+          child: MaterialApp(
+              title: 'Cycle Planner',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
               ),
-            )
-        ),
-      );
+              home:  Scaffold(
+                body: Column(
+                  children: const [
+                    Text('testing'),
+                    CardItem(
+                      itemTitle: 'Test',
+                      itemInfo: 'Test',
+                      imageInfo: 'Test',
+                      placeId: 'Test',
+                      placeInfo: 'Test',
+                    ),
+                  ],
+                ),
+              )
+          ),
+        );
+      });
 
-    });
+      testWidgets(
+          'Testing for widgets created using key', (WidgetTester tester) async {
+        // Define the test key.
+        const testKey = Key('K');
 
-    testWidgets(
-        'Testing for widgets created using key', (WidgetTester tester) async {
-      // Define the test key.
-      const testKey = Key('K');
+        // Build a MaterialApp with the testKey.
+        await tester.pumpWidget(MaterialApp(key: testKey, home: Container()));
 
-      // Build a MaterialApp with the testKey.
-      await tester.pumpWidget(MaterialApp(key: testKey, home: Container()));
-
-      // Find the MaterialApp widget using the testKey.
-      expect(find.byKey(testKey), findsOneWidget);
-    });
+        // Find the MaterialApp widget using the testKey.
+        expect(find.byKey(testKey), findsOneWidget);
+      });
 
 
-    testWidgets('CardItem created successfully', (WidgetTester tester) async {
-      await tester.pumpWidget(cardItem);
-      expect(true, true);
-    });
+      testWidgets('CardItem created successfully', (WidgetTester tester) async {
+        await tester.pumpWidget(cardItem);
+        expect(true, true);
+      });
 
       testWidgets('CardItem created successfully', (WidgetTester tester) async {
         CardItem testCardItem = const CardItem(
-            itemTitle: 'Test',
-            itemInfo: 'Test',
-            imageInfo: 'Test',
-            placeId: 'Test',
-            placeInfo: 'Test',
+          itemTitle: 'Test',
+          itemInfo: 'Test',
+          imageInfo: 'Test',
+          placeId: 'Test',
+          placeInfo: 'Test',
         );
       });
+    });
+
   }
