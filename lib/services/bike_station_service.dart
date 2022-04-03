ec89a675fb7dfe052a8d44b7215403d4d7031b4f
Ammar-alshareef <K19030887@kcl.ac.uk>
2022-04-03 06:50:43 +0100

    Update bike_station_service.dart
    - Add getStations near the user's location

--- lib/services/bike_station_service.dart
+++ lib/services/bike_station_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:cycle_planner/models/bikeStation.dart';
 import 'package:http/http.dart';
 
 /// Class description:
@@ -6,6 +7,20 @@ import 'package:http/http.dart';
 /// from TFL API and filter the data to show available bike stations
 
 class BikeStationService {
+
+  Future<List<BikeStation>> getStations(double? lat, double? lon) async {
+    String url = 'https://api.tfl.gov.uk/Bikepoint?radius=400&lat=$lat&lon=$lon';
+
+    // Request URL with user latitude and longitude
+    Response response = await get(Uri.parse(url));
+    
+    // Get bike stations from TFL JSON
+    dynamic json = jsonDecode(response.body);
+
+    List<dynamic> results = json['places'] as List;
+
+    return results.map((station) => BikeStation.fromJson(station)).toList();
+  }
   
   // Return a list of bike stations closest to the user.
   Future<List> getClosestStations(double? lat, double? lon) async {
