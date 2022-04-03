d2e78c21d859efb0308ddccd48b4f46e9dbfbb7a
Ammar-alshareef <K19030887@kcl.ac.uk>
2022-04-03 21:07:19 +0100

    Update bike_station_model_test.dart
    - Add bike station model tests

--- test/model_tests/bike_station_model_test.dart
+++ test/model_tests/bike_station_model_test.dart
@@ -2,16 +2,65 @@ import 'package:test/test.dart';
 import 'package:cycle_planner/models/bikeStation.dart';
 
 void main() {
-  late dynamic mockBikeStation;
-
   group('Bike station model -', () {
+    late dynamic bikeStationJSON;
+    late BikeStation bikeModel;
+    late String id;
+    late String commonName;
+    late double lat;
+    late double lon;
     setUp(() {
-      mockBikeStation = {
-        "id": "BikePoints_309",
+      bikeStationJSON = {
+        "id": "BikePoints_79",
         "commonName": "Arundel Street, Temple",
         "lat": 51.511726,
         "lon": -0.113855
       };
+
+      id = bikeStationJSON["id"];
+      commonName = bikeStationJSON["commonName"];
+      lat = bikeStationJSON["lat"];
+      lon = bikeStationJSON["lon"];
+
+      bikeModel = BikeStation(id: id, commonName: commonName, lat: lat, lon: lon);
+    });
+
+    test('Required Params passed', () {
+      id = '';
+      commonName = '';
+      lat = 0.0;
+      lon = 0.0;
+
+      BikeStation generateBikeStation = BikeStation(id: id, commonName: commonName, lat: lat, lon: lon);
+
+      expect(generateBikeStation.id, '');
+      expect(generateBikeStation.commonName, '');
+      expect(generateBikeStation.lat, 0.0);
+      expect(generateBikeStation.lon, 0.0);
+    });
+
+    test('Bike station id matches JSON data', () {
+      BikeStation generateBikeStation = BikeStation(id: id, commonName: commonName, lat: lat, lon: lon);
+
+      expect(generateBikeStation.id, bikeModel.id);
+    });
+
+    test('Bike station commonName matches JSON data', () {
+      BikeStation generateBikeStation = BikeStation(id: id, commonName: commonName, lat: lat, lon: lon);
+
+      expect(generateBikeStation.commonName, bikeModel.commonName);
+    });
+
+    test('Bike station lat matches JSON data', () {
+      BikeStation generateBikeStation = BikeStation(id: id, commonName: commonName, lat: lat, lon: lon);
+
+      expect(generateBikeStation.lat, bikeModel.lat);
+    });
+
+    test('Bike station lon matches JSON data', () {
+      BikeStation generateBikeStation = BikeStation(id: id, commonName: commonName, lat: lat, lon: lon);
+
+      expect(generateBikeStation.lon, bikeModel.lon);
     });
   });
 }
