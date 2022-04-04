fa955a6f16439f2697a7655ca47a782e94ea71e7
Ammar-alshareef <K19030887@kcl.ac.uk>
2022-04-04 06:38:57 +0100

    Update iconic_places.dart
    - Fix code format
    - Remove unused imports

--- lib/widgets/iconic_places.dart
+++ lib/widgets/iconic_places.dart
@@ -1,16 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 import 'iconic_card_item.dart';
 
-
-
 class IconicScreen extends StatefulWidget {
-   const IconicScreen({Key? key,}) : super(key: key);
-
+  const IconicScreen({Key? key,}) : super(key: key);
 
   @override
   _IconicScreenState createState() => _IconicScreenState();
@@ -26,30 +19,28 @@ class _IconicScreenState extends State<IconicScreen> {
         title: const Text("Iconic places"),
         backgroundColor: Colors.redAccent,
       ),
-
-    body: StreamBuilder<QuerySnapshot>(
+      body: StreamBuilder<QuerySnapshot>(
         stream: firestore.collection("iconic-places").snapshots(),
         builder: (context, snapshot){
           if(!snapshot.hasData) {
             return const Text('Loading ...');
           }
           return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (context, index) {
-                String itemTitle = snapshot.data!.docs[index]['name'] ;
-                String itemInfo = snapshot.data!.docs[index]['address'];
-                String placeInfo = snapshot.data!.docs[index]['place_info'];
-                String imageInfo = snapshot.data!.docs[index]['image'];
-                String placeId = snapshot.data!.docs[index]['place_id'];
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              String itemTitle = snapshot.data!.docs[index]['name'] ;
+              String itemInfo = snapshot.data!.docs[index]['address'];
+              String placeInfo = snapshot.data!.docs[index]['place_info'];
+              String imageInfo = snapshot.data!.docs[index]['image'];
+              String placeId = snapshot.data!.docs[index]['place_id'];
 
-                return CardItem(itemTitle: itemTitle, itemInfo: itemInfo, imageInfo: imageInfo, placeId: placeId, placeInfo: placeInfo );
-          });
-          },
-        ),
+              return CardItem(itemTitle: itemTitle, itemInfo: itemInfo, imageInfo: imageInfo, placeId: placeId, placeInfo: placeInfo );
+            }
+          );
+        },
+      ),
     );
   }
-
-
 }
 
 
