ded9a5e2948095341acf1f961ec75347bc640c56
Ammar-alshareef <K19030887@kcl.ac.uk>
2022-04-05 04:01:02 +0100

    Update info_page.dart
    - Redesign info_page

--- lib/widgets/info_page.dart
+++ lib/widgets/info_page.dart
@@ -1,55 +1,38 @@
 import 'package:flutter/material.dart';
 
-
 class InfoPage extends StatefulWidget {
-  String name = "";
-  d(nme){
-    name = nme;
-    IconData icon;
-  }
   InfoPageState createState() => new InfoPageState();
 }
-
 
 class InfoPageState extends State<InfoPage> {
 
   final List<String> listItems = [
-  ('Step 1: Choose one or multiple destinations!'),
-  ('Step 2: Click on the direction icon to view your route!'),
-  ('Step 3: Start navigation by clicking the fourth icon'),
-  ('Step 4: Enjoy your journey through London! '),
-  ('Extra Features'),
-  ('Pick a group size! or even manage your journey through our journey planner!'),
-  ('Enjoy our most iconic places that will guide you around the city! '),
+    ('Step 1: Choose one or multiple destinations!'),
+    ('Step 2: Click on the direction icon to view your route!'),
+    ('Step 3: Start navigation by clicking the fourth icon'),
+    ('Step 4: Enjoy your journey through London!'),
+    ('Extra Features'),
+    ('Pick a group size or even manage your journey through our journey planner!'),
+    ('Enjoy our most iconic places that will guide you around the city!'),
+    ('Create an account in order to save your favourite destinations!'),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Info Page"),
-          backgroundColor: Colors.blueGrey,
-        ),
-            body: Column(children: <Widget>[
-              Text(
-             "Guide",
-             style: TextStyle(
-                 fontSize:50,
-                color: Colors.blueAccent,
-               fontWeight: FontWeight.w500,
-             )
-              ),
-              ListAppWidget(listItems),
-            ],
-            )
-        );
+      appBar: AppBar(
+        title: const Text("How to use the app?"),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: ListAppWidget(listItems)
+    );
   }
 }
 
 //The widget to be tested
 class ListAppWidget extends StatefulWidget {
-  List<String> listItems =[];
-  ListAppWidget(List<String> lst){
+  List<String> listItems = [];
+  ListAppWidget(List<String> lst) {
     listItems.clear(); listItems.addAll(lst);
   }
 
@@ -60,21 +43,40 @@ class ListAppWidget extends StatefulWidget {
 class ListAppWidgetState extends State<ListAppWidget> {
   @override
   Widget build(BuildContext context) {
-    return(
-        Container(
-            width: MediaQuery.of(context).size.width,height:MediaQuery.of(context).size.height/2,
-            padding: EdgeInsets.only(left:10,right:10,top:20,bottom:20),
-            child: ListView.builder(
-                key:Key("myListView"),
-                itemCount: widget.listItems.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(child:Container(
-                      height:100,
-                      child:Center(child:Text( widget.listItems[index], key:Key("listViewText"))))
-                  );
-                }
-            )
-        )
+    return Container(
+      child: ListView.builder(
+        key: Key("myListView"),
+        itemCount: widget.listItems.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            padding: EdgeInsets.all(5),
+            child: Card(
+              shadowColor: Colors.black,
+              margin: EdgeInsets.all(7),
+              child: Container(
+                color: Colors.blue[700],
+                height: 120,
+                child: Center(
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(7, 10, 5, 10),
+                    child: Text(
+                      widget.listItems[index],
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 0.5,
+                      ),
+                      key:Key("listViewText")
+                    ),
+                  )
+                )
+              )
+            ),
+          );
+        }
+      )
     );
   }
 }
