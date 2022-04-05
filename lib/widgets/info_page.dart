c3e8bae423338661798b3a73ae0f773fde80e918
HadwaH <79116483+HadwaH@users.noreply.github.com>
2022-04-05 05:30:08 +0400

    Fix list view info

--- lib/widgets/info_page.dart
+++ lib/widgets/info_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 
 class InfoPage extends StatefulWidget {
-  @override
   String name = "";
   d(nme){
     name = nme;
@@ -13,104 +12,75 @@ class InfoPage extends StatefulWidget {
 
 
 class InfoPageState extends State<InfoPage> {
-  // List<String> listItems =[
-  //   ("Step 1 : take ammars dick"), ("Step 2: drag it along asscrack"), ("Step: 3 shove it back in his ass")
-  // ];
 
-  final List<String> steps = <String>[
-    'Step 1: Choose your destination through search ',
-    'Step 2: View your guided route ',
-    'Step 3: Find your waypoitns ',
-    'Step 4: ddd ',
-    'Step: 5 dddd'
+  final List<String> listItems = [
+  ('Step 1: Choose one or multiple destinations!'),
+  ('Step 2: Click on the direction icon to view your route!'),
+  ('Step 3: Start navigation by clicking the fourth icon'),
+  ('Step 4: Enjoy your journey through London! '),
+  ('Extra Features'),
+  ('Pick a group size! or even manage your journey through our journey planner!'),
+  ('Enjoy our most iconic places that will guide you around the city! '),
   ];
-  final List<IconData> icons = <IconData>[
-    Icons.search,
-    Icons.search,
-    Icons.favorite,
-    Icons.favorite,
-    Icons.search
-  ];
-
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Info Page"),
-          backgroundColor: Colors.redAccent,
+          backgroundColor: Colors.blueGrey,
         ),
             body: Column(children: <Widget>[
-              Text("Guide, "
-                  , style: TextStyle(fontSize:20)),
-              ListAppWidget(steps, icons),
-            ],)
+              Text(
+             "Guide",
+             style: TextStyle(
+                 fontSize:50,
+                color: Colors.blueAccent,
+               fontWeight: FontWeight.w500,
+             )
+              ),
+              ListAppWidget(listItems),
+            ],
+            )
         );
-
   }
-
 }
 
 //The widget to be tested
 class ListAppWidget extends StatefulWidget {
-  List<String> steps =[];
-  List<IconData> icons =[];
-  ListAppWidget(List<String> lst, List<IconData> icons){
-    steps.clear(); steps.addAll(lst);
+  List<String> listItems =[];
+  ListAppWidget(List<String> lst){
+    listItems.clear(); listItems.addAll(lst);
   }
 
   @override
   ListAppWidgetState createState() => new ListAppWidgetState();
 }
 
-
 class ListAppWidgetState extends State<ListAppWidget> {
   @override
   Widget build(BuildContext context) {
-    return (
+    return(
         Container(
-            width: MediaQuery
-                .of(context)
-                .size
-                .width, height: MediaQuery
-            .of(context)
-            .size
-            .height / 2,
-            padding: EdgeInsets.only(left: 10, right: 10, top: 20, bottom: 20),
+            width: MediaQuery.of(context).size.width,height:MediaQuery.of(context).size.height/2,
+            padding: EdgeInsets.only(left:10,right:10,top:20,bottom:20),
             child: ListView.builder(
-                key: Key("myListView"),
-                itemCount: widget.steps.length,
-                itemBuilder: (BuildContext ctxt, int index) {
-                  return Card(
-                      child: ListTile(
-                        leading: Icon(widget.icons[index]),
-                        title: Text(widget.steps[index]),
-                      )
-
+                key:Key("myListView"),
+                itemCount: widget.listItems.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(child:Container(
+                      height:100,
+                      child:Center(child:Text( widget.listItems[index], key:Key("listViewText"))))
                   );
-
-                  //   Card(
-                  //     child:Container(
-                  //     height:100,
-                  //     child:Center(child:Text( widget.steps[index], key:Key("listViewText")),))
-                  //
-                  //
-                  // );
-                })
+                }
+            )
         )
     );
   }
 }
 
 
-    //     children: <Widget>[
-    //     Icon(icons[index]['icon']),
-    // SizedBox(width: 20.0),
-    // Text(_categories[index]['name']),
-    // ],
-    // );
+
 
 
 
