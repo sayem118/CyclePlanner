61f0c159072a5f37829773880a6d27b6ae61d67b
Ammar-alshareef <K19030887@kcl.ac.uk>
2022-04-05 10:11:36 +0100

    Update search_page_test.dart
    - Fix setUp()

--- test/widget_tests/search_page_test.dart
+++ test/widget_tests/search_page_test.dart
@@ -1,14 +1,14 @@
 import 'dart:io';
-
-import 'package:cycle_planner/processes/application_processes.dart';
+import 'package:cycle_planner/models/place_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cycle_planner/widgets/search_page.dart';
 import 'package:provider/provider.dart';
+import 'package:cycle_planner/processes/application_processes.dart';
 import 'searchPageTemplate.dart';
 
 void main() {
-  late dynamic searchPage;
+  late var searchPage;
 
   setUp(() {
     HttpOverrides.global = null;
