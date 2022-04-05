1d52595b6db3b07f6374d92e2e648d60ecec2920
Ammar-alshareef <K19030887@kcl.ac.uk>
2022-04-05 08:25:41 +0100

    Add search_page tests

--- test/widget_tests/search_page_test.dart
+++ test/widget_tests/search_page_test.dart
@@ -1,11 +1,89 @@
+import 'dart:io';
+
+import 'package:cycle_planner/processes/application_processes.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:cycle_planner/widgets/search_page.dart';
+import 'package:provider/provider.dart';
+import 'searchPageTemplate.dart';
 
 void main() {
+  late dynamic searchPage;
 
-  testWidgets('SearchPage created successfully', (WidgetTester tester) async {
-    SearchPage testNavBar = SearchPage(
+  setUp(() {
+    HttpOverrides.global = null;
+    searchPage = ChangeNotifierProvider<ApplicationProcesses>(
+      create: (context) => ApplicationProcesses(),
+      child: DelegatePage(delegate: SearchPage())
     );
   });
 
+  group('Search page -', () {
+
+    testWidgets('Search delegate loads', (WidgetTester tester) async {
+      await tester.pumpWidget(searchPage);
+
+      await tester.tap(find.byTooltip('Search'));
+
+      await tester.pump();
+
+      await tester.pump(const Duration(milliseconds: 300));
+
+      SearchPage().query = 'test';
+
+      await tester.pumpAndSettle();
+    });
+
+    testWidgets('Search bar loads', (WidgetTester tester) async {
+      await tester.pumpWidget(searchPage);
+
+      await tester.tap(find.byTooltip('Search'));
+
+      await tester.pump();
+
+      await tester.pump(const Duration(milliseconds: 300));
+
+      SearchPage().query = 'test';
+
+      final TextField textField = tester.widget<TextField>(find.byType(TextField));
+
+      expect(
+        textField.controller!.selection,
+        TextSelection(
+          baseOffset: SearchPage().query.length,
+          extentOffset: SearchPage().query.length,
+        ),
+      );
+    });
+
+    testWidgets('close search page and return query result', (WidgetTester tester) async {
+      final List<String> selectedResults = <String>[];
+
+      await tester.pumpWidget(
+        ChangeNotifierProvider<ApplicationProcesses>(
+          create: (context) => ApplicationProcesses(),
+          child: DelegatePage(delegate: SearchPage(), results: selectedResults,)
+        )
+      );
+
+      await tester.tap(find.byTooltip('Search'));
+
+      await tester.pumpAndSettle();
+
+      expect(selectedResults, hasLength(0));
+
+      final TextField textField = tester.widget(find.byType(TextField));
+      textField.controller?.value = TextEditingValue(text: 'Query result');
+      expect(textField.focusNode!.hasFocus, isTrue);
+
+      // Close search
+      await tester.tap(find.byIcon(Icons.arrow_back));
+      await tester.pumpAndSettle();
+
+      expect(find.text('HomeBody'), findsOneWidget);
+      expect(find.text('HomeTitle'), findsOneWidget);
+      expect(find.text('Suggestions'), findsNothing);
+      expect(selectedResults, <String>['Query result']);
+    });
+  });
 }
