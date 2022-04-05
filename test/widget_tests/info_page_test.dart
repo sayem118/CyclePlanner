1a99364e45d6b20999bf2f931ac5f2565e189375
Ammar-alshareef <K19030887@kcl.ac.uk>
2022-04-05 06:58:21 +0100

    Fix info_page_test

--- test/widget_tests/info_page_test.dart
+++ test/widget_tests/info_page_test.dart
@@ -1,16 +1,29 @@
 import 'package:cycle_planner/widgets/info_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-// import our widget
 
 void main() {
-  testWidgets('testing listview widget', (WidgetTester tester) async {
-    // the items to add to list
-    List<String> listItems = ["item 1", "item 2", "item 3"];
+  late Widget infoPage;
 
-    //start the widget
-    await tester.pumpWidget(
-        MaterialApp(home: ListAppWidget(listItems))
+  setUp(() {
+    infoPage = MaterialApp(
+      home: InfoPage()
     );
   });
-  }
+
+  group('Info page -', () {
+    testWidgets('info page loads', (WidgetTester tester) async {
+      await tester.pumpWidget(infoPage);
+
+      expect(true, true);
+    });
+
+    testWidgets('User can see 4 cards', (WidgetTester tester) async {
+      await tester.pumpWidget(infoPage);
+
+      Finder cards = find.byType(Card);
+
+      expect(cards, findsNWidgets(4));
+    });
+  });
+}
